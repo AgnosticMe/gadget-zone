f409f9c401c35c16a717e08d6ab28bbe5946767b
agnostic-me <official.ks00@gmail.com>
2021-05-06 18:54:47 +0530

    create single product detail page

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from category.models import Category
 
 # Create your models here.
@@ -13,7 +14,10 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    
 
+    def get_url(self):
+        return reverse('store:product_details', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,7 @@ app_name = 'store'
 urlpatterns = [
     path('', views.store, name='store'),
     path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_details,
+    name='product_details'),
+
 ]
