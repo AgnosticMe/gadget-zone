9734f767005bd4b79a86e9b2d1cee5b446f3740b
agnostic-me <official.ks00@gmail.com>
2021-06-19 15:31:17 +0530

    install django-admin-thumbnail & view thumbnails in admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Product, ProductGallery
+import admin_thumbnails
 
-
+@admin_thumbnails.thumbnail('image')
 class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
     extra = 1
 
 @admin.register(Product)
+@admin_thumbnails.thumbnail('images')
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['product_name', 'price', 'category', 'is_available' , 'stock', 'date_modified' ]
     prepopulated_fields = {'slug': ('product_name',)}
