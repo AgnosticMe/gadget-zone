d5a07571544deff0effd866167feee67f081407e
agnostic-me <official.ks00@gmail.com>
2021-06-19 15:18:16 +0530

    create product gallery model & attach as inline to product model

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductGallery
 
-# Register your models here.
+
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['product_name', 'price', 'category', 'is_available' , 'stock', 'date_modified' ]
     prepopulated_fields = {'slug': ('product_name',)}
     ordering = ['-date_created']
+    inlines = [ProductGalleryInline]
+
+
+
+admin.site.register(ProductGallery)
+
+
+
--- store/models.py
+++ store/models.py
@@ -21,3 +21,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'Product Gallery'
+        verbose_name_plural = 'Product Gallery'
