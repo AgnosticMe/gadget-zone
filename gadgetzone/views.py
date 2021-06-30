b837116714010db456920e51f0ac69dbb986e3ae
agnostic-me <official.ks00@gmail.com>
2021-06-30 09:43:06 +0530

    display review stars on home page & modify title of different templates

--- gadgetzone/views.py
+++ gadgetzone/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from store.models import Product
+from store.models import ReviewAndRating
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by('-date_created')
+
+    # Get the reviews of products   
+    for product in products:
+        reviews = ReviewAndRating.objects.filter(product_id=product.id, status=True)
+
     context = {
-        'products': products
+        'products': products,
+        'reviews': reviews,
     }
     return render(request, 'home.html', context)
